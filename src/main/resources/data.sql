d93a8d5cb1c58a41b688d1a40cd01b971080e23c
ATMatias22 <matias.torrez@davinci.edu.ar>
2024-05-28 02:17:37 -0300

    Modificando archivos para poder subirlo a railway
    Sacamos los comentarios en el .properties porque tenian algunos caracteres que no aceptaban ISO-8859-1
    Tambien agregamos las propiedades necesarias para railway (server.address y server.port)
    Agregamos en dockerfile el comando de maven, ya que por alguna razon tira un error al deployarlo en railway

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,16 +1,17 @@
 
-INSERT INTO Producto (nombre,descripcion,precio,habilitado)
-VALUES ('Producto 1',"Este producto es de prueba, 1111", 5000, true), ('Producto 2',"Este producto es de prueba, 2222", 7000, true);
-
+INSERT INTO producto (nombre, descripcion, precio, habilitado)
+VALUES
+('Producto 1', 'Este producto es de prueba, 1111', 5000, true),
+('Producto 2', 'Este producto es de prueba, 2222', 7000, true);
 
 INSERT INTO Stock (fk_producto,telefono_comprador,estado)
-VALUES (1,null,"A_LA_VENTA"), (1,null,"A_LA_VENTA"), (2,null,"A_LA_VENTA");
+VALUES (1,null,'A_LA_VENTA'), (1,null,'A_LA_VENTA'), (2,null,'A_LA_VENTA');
 
 
 INSERT INTO dia_disponible (dia,horario_inicio,horario_fin)
-VALUES ("Lunes","08:00","12:00"), ("Lunes","14:00","18:00"),
-("Martes","08:00","12:00"), ("Martes","14:00","18:00"),
-("Miercoles","08:00","12:00"), ("Miercoles","14:00","18:00"),
-("Jueves","08:00","12:00"), ("Jueves","14:00","18:00"),
-("Viernes","08:00","12:00"), ("Viernes","14:00","18:00");
+VALUES ('Lunes','08:00','12:00'), ('Lunes','14:00','18:00'),
+('Martes','08:00','12:00'), ('Martes','14:00','18:00'),
+('Miercoles','08:00','12:00'), ('Miercoles','14:00','18:00'),
+('Jueves','08:00','12:00'), ('Jueves','14:00','18:00'),
+('Viernes','08:00','12:00'), ('Viernes','14:00','18:00');
 
